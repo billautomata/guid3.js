4562225b6d51864543c6f01d0e6829f78e94b79a
billautomata <bill@evident.io>
2014-09-26 13:21:56 -0700

    0.6.1

---
+++ build/apidocs/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Button",
+        "Slider"
+    ],
+    "modules": [
+        "GUId3"
+    ],
+    "allModules": [
+        {
+            "displayName": "GUId3",
+            "name": "GUId3",
+            "description": "UI toolkit for D3js"
+        }
+    ]
+} };
+});
