33880f05c060d3af9a6454ce827108f7d0dfb78b
billautomata <bill@evident.io>
2014-10-03 10:15:48 -0700

    big changes to bring the buttons in line

---
+++ examples/example5.js
@@ -0,0 +1,20 @@
+var button = new GUId3.Button()
+button.cssClass('example5')
+button.width(100).height(50)
+button.labelOn('zomgon').labelOff('zomgoff')
+button.callback(function(v){
+  if(v){
+    d3.select('div#example5').style('background-color', '#406573')
+  } else {
+    d3.select('div#example5').style('background-color', 'black')
+  }
+})
+
+// create the container, or select one
+var container = d3.select('div#example5')
+  .append('svg').attr('width', 500).attr('height', 300)
+
+// create the button
+button.create(container.append('g')
+  .attr('transform','translate(10,10)'))
+button.setValue(true)
---
+++ examples/example6.js
@@ -0,0 +1,21 @@
+var button = new GUId3.Button()
+button.cssClass('example6')
+button.width(150).height(50)
+button.labelOn('zomgon').labelOff('zomgomentary')
+button.type('momentary')
+button.callback(function(v){
+  if(v){
+    d3.select('div#example6').style('background-color', '#406573')
+  } else {
+    d3.select('div#example6').style('background-color', 'white')
+  }
+})
+
+// create the container, or select one
+var container = d3.select('div#example6')
+  .append('svg').attr('width', 500).attr('height', 300)
+
+// create the button
+button.create(container.append('g')
+  .attr('transform','translate(10,10)'))
+button.setValue(true)
