568fb5627f30a86734ce8f5bcd8cc4f667e9ea36
billautomata <bill@evident.io>
2014-10-03 08:47:23 -0700

    added styled slider example

---
+++ examples/example3.js
@@ -0,0 +1,19 @@
+var slider = new GUId3.Slider()
+
+slider.cssClass('example3')
+
+slider.width(300).height(20).label('styled slider')
+
+// set up the scale using a d3 scale!
+var my_scale = d3.scale.linear().domain([1,100]).range([303,909])
+slider.scale(my_scale)
+
+// create the container, or select one
+var container = d3.select('div#example3')
+  .append('svg').attr('width', 512).attr('height', 32)
+
+// create the slider
+slider.create(container)
+
+// set the value of the slider
+slider.setValue(808.1)
